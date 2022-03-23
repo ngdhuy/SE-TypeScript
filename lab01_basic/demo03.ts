46a2c610fbac41afad36ede729d3080caf75ddbb
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-03-23 18:14:14 +0700

    feat(basic): data-type special unknown of typescript

---
+++ lab01_basic/demo03.ts
@@ -0,0 +1,28 @@
+/**
+ * ? Special data-type
+ */
+
+//! Unknown
+
+let not_sure: unknown = 3;
+console.log("not_sure =", not_sure, '\t ->', typeof not_sure);
+
+not_sure = "Hello world!!!";
+console.log("not_sure =", not_sure, '\t ->', typeof not_sure);
+
+not_sure = true; 
+console.log("not_sure =", not_sure, '\t ->', typeof not_sure);
+
+//! special case of Unknown
+const maybe: unknown = true;
+// const num: number = maybe;
+
+if(maybe === true) {
+  const value_boolean: boolean = maybe;
+  console.log("value =", value_boolean, "\t ->", typeof maybe);
+}
+
+if(typeof maybe === "string") {
+  const value_string: string = maybe;
+  console.log("value =", value_string, "\t ->", typeof maybe);
+}
