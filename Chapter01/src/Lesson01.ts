f9ce5122e197e54e07fc790cc9a11b3cfe662b17
ndhuy <ndhuy@fit.hcmus.edu.vn>
2022-03-28 15:28:17 +0700

    init: config typescript project

---
+++ Chapter01/src/Lesson01.ts
@@ -0,0 +1,14 @@
+//@ts-check
+const i : number = 10
+
+/**
+ * Sum two numbers
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+const sum = (a : number, b : number) => {
+  return a + b
+}
+
+console.log(i)
