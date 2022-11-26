9c851170adf8479f9b1a0b6576998d0ef131bff5
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-11-27 04:35:25 +0700

    feat(mvc): add route and view

--- lab04_express/demo02_mvc/src/index.ts
+++ lab04_express/demo02_mvc/src/index.ts
@@ -1,24 +1,30 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import * as sessionAuth from "./middleware/sessionAuth";
+import * as routes from "./routes";
 
 // initialize configuration
 dotenv.config();
 
-const app = express();
+// port is now available to the Node.js runtime
+// as if it were an environment variable
 const port = process.env.SERVER_PORT;
 
-// configure express to use EJS
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+// Configure session auth
+sessionAuth.register(app);
+
+// Configure routes
+routes.register(app);
 
 // start the express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`server  at http://localhost:${port}`);
 });
---
+++ lab04_express/demo02_mvc/src/middleware/sessionAuth.ts
@@ -0,0 +1,14 @@
+import session from "express-session";
+
+export const register = (app: any) => {
+
+
+  // Configure Express to use authentication sessions
+  app.use(
+    session({
+      resave: true,
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET,
+    })
+  );
+};
